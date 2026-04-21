4a0ffed35f7e4655b06b6f146ef8b41d2e14bc0e
Andy de Oliveira <elderromero565@gmail.com>
2026-04-20 22:40:54 -0300

    515d155

--- src/componentes/header/header.jsx
+++ src/componentes/header/header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./Header.css";
+import "./header.css";
 import logo from "../../logo.png";
 
 export default function Header() {
